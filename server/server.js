b74f45e8f997cf5bde94c0e86c289bcd3d186503
Bhagyashree <120381687+bhagyashree4496@users.noreply.github.com>
2025-01-05 16:40:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  (
+    origin: ["https://shopsphere-x6t84b8cx-bhagyashree4496s-projects.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  )
+));
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, "../client/build")));
 
