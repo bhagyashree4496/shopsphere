1d2a4bef6ae7cfeea602c1db0c611875eff7c142
Bhagyashree <bhagyashree4496@gmail.com>
2025-02-19 09:53:27 +0530

    fixed bug in favorites

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,13 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../context/ProductContext";
 import { IoClose } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { updateUserDetails } from "../api/api";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Favorites = () => {
   const { state, dispatch } = useContext(ProductContext);
-  const navigate = useNavigate();
-  const handleRemove = (id) => {
-    // dispatch({ type: "REMOVE_FROM_CART", payload: id });
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+  const handleRemove = async (p) => {
+    dispatch({ type: "ADD_REMOVE_FAVORITES", payload: p });
+    if (user) {
+      const remaining = state.favorites.filter((pro) => pro._id !== p._id);
+      await updateUserDetails(user.uid, {
+        favorites: [...remaining],
+      });
+    }
   };
 
   return (
@@ -16,10 +32,7 @@ const Favorites = () => {
       {state.favorites?.length > 0 ? (
         <div className="space-y-4">
           {state.favorites?.map((p) => (
-            <div
-              className="space-y-4"
-              onClick={() => navigate(`/product/${p?.id}`)}
-            >
+            <div className="space-y-4">
               <div
                 key={p?.id}
                 className="flex items-center bg-white p-4 rounded-md shadow-md relative"
@@ -35,7 +48,7 @@ const Favorites = () => {
                   <p className="text-md text-gray-600">Price: ₹{p?.cost}</p>
                 </div>
                 <button
-                  onClick={() => handleRemove(p?.id)}
+                  onClick={() => handleRemove(p)}
                   className="absolute text-white top-2 right-2 text-[12px] rounded-full bg-[#a749ff] p-1"
                 >
                   <IoClose />
